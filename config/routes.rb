c09775f0cba75bf48af4d8d4b94f3ba68ef5ff09
motoki-adachi <zyx.wvu43@icloud.com>
2020-04-11 19:11:17 +0900

    create_google_search
    グーグルサイト内検索の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :new, :create, :show] do
     collection do
       get :top
-      get :search
     end
   end
   resources :users, only: [:show]
