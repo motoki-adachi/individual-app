f03ada8185cda4f259f1db6489c9e71b6a696b09
motoki-adachi <zyx.wvu43@icloud.com>
2020-04-20 20:16:12 +0900

    refactoring

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,18 +1,14 @@
 class RegistersController < ApplicationController
 
   def register
-
     @book = Book.find(params[:id])
     @register = Register.new
-    
   end
 
   def register_create
-
     @book = Book.find(params[:id])
     @exist = Register.find_by(book_id: params[:id], user_id: current_user.id)
     @register = Register.new(register_params)
-
     if @exist == nil
       @register.save
       redirect_to user_path(current_user)
@@ -21,20 +17,17 @@ class RegistersController < ApplicationController
       @register.save
       redirect_to user_path(current_user)
     end
-
   end
 
 private
 
-def register_params
-
-  params.require(:register)
-        .permit(:status)
-        .merge(user_id: current_user.id, 
-               book_id: @book.id,
-               genre_id: @book.genre_id
-              )
-
-end
+  def register_params
+    params.require(:register)
+          .permit(:status)
+          .merge(user_id: current_user.id, 
+                book_id: @book.id,
+                genre_id: @book.genre_id
+                )
+  end
 
 end
